775cbd161dfe2cb40feeee2e70986e060b476dd5
rqroz <rqroz1@gmail.com>
2018-06-28 22:05:20 -0300

    removed unnecessary files

--- polygons/website/views/modules.py
+++ polygons/website/views/modules.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
-
-from website.models import *
-from website.forms import *
